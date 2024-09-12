ef7b89cb0cc1b2310cc0dcedb56f4de4b43f50a1
mrkirill046 <mrkirill046@yandex.ru>
2024-09-12 13:23:48 +0300

    first commit. created project

---
+++ src/test/java/com/qwy_games/fllaf_messenger/messageservice/MessageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qwy_games.fllaf_messenger.messageservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
